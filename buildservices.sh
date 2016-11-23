50b267c83292f2eca914108cb3719126c6174b9b
chadrc <chadrcollins91@gmail.com>
2016-11-23 18:29:17 -0500

    Changed build script to build with docker. Specific service may now be selected and built individually.

--- buildservices.sh
+++ buildservices.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-SERVICES=(
-loginservice
-loggerservice
-cardgameservice
-)
+PARAM_COUNT=$#
+if [ ${PARAM_COUNT} -ge 1 ] ; then
+    COUNT=0
+    SERVICES=()
+    for SERVICE in $@; do
+        SERVICES[${COUNT}]=${SERVICE}
+        COUNT+=1
+    done
+else
+    SERVICES=(
+    loginservice
+    loggerservice
+    cardgameservice
+    )
+fi
 
 SERVICE_ROOT=github.com/chadrc/microservices
 
 for SERVICE in ${SERVICES[@]}; do
-    echo Building ${SERVICE}
-    SERVICE_PATH=${SERVICE_ROOT}/${SERVICE}
-    echo "      "Path: ${SERVICE_PATH}
-    EXPORT_PATH=${GOPATH}/src/${SERVICE_ROOT}/${SERVICE}/build/${SERVICE}.service
-    go build -o ${EXPORT_PATH} ${SERVICE_PATH}
-    echo "      "Build: ${EXPORT_PATH}
+    if [ -d "./"${SERVICE} ]; then
+        echo Building ${SERVICE}
+        cp ./Dockerfile ./${SERVICE}/Dockerfile
+        echo | sudo docker build -t=${SERVICE} ./${SERVICE}
+        rm -f ./${SERVICE}/Dockerfile
+    else
+        echo ${SERVICE} "does not exist."
+    fi
 done
