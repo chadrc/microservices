36f6673ec3e6038c5d7933e8b51d56d6ff97aff4
chadrc <chadrcollins91@gmail.com>
2016-11-23 11:59:05 -0500

    Begain implementing login service.

--- loginservice/loginservice.go
+++ loginservice/loginservice.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"crypto/sha256"
+	"time"
+	"math/rand"
+)
+
+type User struct {
+	name        string
+	password    string
+	accessToken string
+}
+
+func (user *User) GetName() (username string) {
+	username = user.name
+	return
+}
+
+var usersByName map[string]*User = make(map[string]*User)
+var loggedInUsers map[string]*User = make(map[string]*User)
 
 func main() {
-	fmt.Println("Login Service")
+	http.HandleFunc("/register", registerNewUser)
+	http.HandleFunc("/login", loginUser)
+	http.HandleFunc("/logout", logoutUser)
+	http.HandleFunc("/checktoken", checkAccessToken)
+	fmt.Println("Login service listening on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func registerNewUser(response http.ResponseWriter, request *http.Request) {
+	name := request.URL.Query().Get("name")
+	if name == "" {
+		http.Error(response, "Name required.", http.StatusBadRequest)
+		return;
+	}
+
+	password := request.URL.Query().Get("password")
+	if password == "" {
+		http.Error(response, "Password required.", http.StatusBadRequest)
+		return;
+	}
+
+	_, userExists := usersByName[name]
+	if userExists {
+		http.Error(response, "User with that name already exists.", http.StatusBadRequest)
+		return;
+	}
+
+	var user User
+	user.name = name
+
+	var passSha = sha256.New()
+	passSha.Write([]byte(password))
+	user.password = string(passSha.Sum(nil))
+
+	var tokenSha = sha256.New()
+	tokenSha.Write([]byte(time.Now().String() + string(rand.Int63())))
+	user.accessToken = string(tokenSha.Sum(nil))
+
+	usersByName[user.name] = &user
+
+	response.Write([]byte("{message: 'Registration successful.', accessToken: '" + user.accessToken + "'}"))
+}
+
+func loginUser(response http.ResponseWriter, request *http.Request) {
+
+}
+
+func logoutUser(response http.ResponseWriter, request *http.Request) {
+
+}
+
+func checkAccessToken(response http.ResponseWriter, request *http.Request) {
+
 }
