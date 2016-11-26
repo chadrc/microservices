23e36e93b268fefdc32f27fdbd7b75f7dcade06a
chadrc <chadrcollins91@gmail.com>
2016-11-26 13:37:11 -0500

    Reworked user and session tables schema. Added function for creating a user.

--- postgresimage/services-tables.sql
+++ postgresimage/services-tables.sql
@@ -1,15 +1,17 @@
-CREATE TABLE users (
+CREATE TABLE user_accounts (
+  id SERIAL NOT NULL UNIQUE,
   name VARCHAR(128) PRIMARY KEY,
   password VARCHAR(64) NOT NULL,
-  sessionToken VARCHAR(64),
+  accessToken VARCHAR(64),
+  currentSession INTEGER NOT NULL ,
   created TIMESTAMP NOT NULL DEFAULT now()
 );
 
-CREATE TABLE session_tokens (
-  username VARCHAR(128) NOT NULL,
-  token VARCHAR(64) NOT NULL,
+CREATE TABLE user_sessions (
+  id INTEGER NOT NULL,
+  userId INTEGER NOT NULL,
   lastcheck TIMESTAMP NOT NULL DEFAULT now(),
   created TIMESTAMP NOT NULL DEFAULT now(),
-  PRIMARY KEY (username, token),
-  FOREIGN KEY (username) REFERENCES users (name)
+  PRIMARY KEY (id, userId),
+  FOREIGN KEY (userId) REFERENCES user_accounts (id)
 );
