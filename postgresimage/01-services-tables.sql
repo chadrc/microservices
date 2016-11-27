36f717de6e1762bdddb2fb07165564111649918d
chadrc <chadrcollins91@gmail.com>
2016-11-26 20:27:40 -0500

    Renamed table columns for consistency.

--- postgresimage/01-services-tables.sql
+++ postgresimage/01-services-tables.sql
@@ -2,16 +2,16 @@ CREATE TABLE user_accounts (
   id SERIAL NOT NULL UNIQUE,
   name VARCHAR(255) PRIMARY KEY,
   password VARCHAR(64) NOT NULL,
-  accessToken VARCHAR(64) UNIQUE,
-  currentSession INTEGER NOT NULL,
+  access_token VARCHAR(64) UNIQUE,
+  current_session INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT now()
 );
 
 CREATE TABLE user_sessions (
   id INTEGER NOT NULL,
-  userId INTEGER NOT NULL,
-  lastcheck TIMESTAMP NOT NULL DEFAULT now(),
+  user_id INTEGER NOT NULL,
+  last_check TIMESTAMP NOT NULL DEFAULT now(),
   created TIMESTAMP NOT NULL DEFAULT now(),
-  PRIMARY KEY (id, userId),
-  FOREIGN KEY (userId) REFERENCES user_accounts (id)
+  PRIMARY KEY (id, user_id),
+  FOREIGN KEY (user_id) REFERENCES user_accounts (id)
 );
--- postgresimage/02-services-functions.sql
+++ postgresimage/02-services-functions.sql
@@ -1,19 +1,25 @@
+CREATE OR REPLACE FUNCTION username_exists(username VARCHAR)
+  RETURNS VARCHAR(255)
+    AS $$
+    SELECT name FROM user_accounts WHERE name=username;
+    $$
+    LANGUAGE SQL;
+
 CREATE OR REPLACE FUNCTION create_user(username VARCHAR, pass VARCHAR, token VARCHAR)
   RETURNS user_accounts
-    as $$
-      INSERT INTO user_accounts ("name", "password", "accesstoken", "currentsession") VALUES (username, pass, token, 1);
-      INSERT INTO user_sessions ("userid", "id") VALUES ((SELECT id FROM user_accounts WHERE "name"=username), 1);
-      SELECT * FROM user_accounts WHERE "name"=username;
+    AS $$
+      INSERT INTO user_accounts (name, password, access_token, current_session) VALUES (username, pass, token, 1);
+      INSERT INTO user_sessions (user_id, id) VALUES ((SELECT id FROM user_accounts WHERE name=username), 1);
+      SELECT * FROM user_accounts WHERE name=username;
     $$
     LANGUAGE SQL;
 
 CREATE OR REPLACE FUNCTION ping_session_and_get_user_with_access_token(token VARCHAR)
   RETURNS TABLE (
-    user_id INTEGER,
-    user_name VARCHAR(255),
-    session_id INTEGER
+    userId INTEGER,
+    sessionId INTEGER
   )
-    as $$
+    AS $$
       DECLARE
         found_userId INTEGER := 0;
         found_sessionId INTEGER := 0;
@@ -22,44 +28,44 @@ CREATE OR REPLACE FUNCTION ping_session_and_get_user_with_access_token(token VAR
         found_user user_accounts;
         user_session user_sessions;
       BEGIN
-        IF EXISTS (SELECT id FROM user_accounts WHERE accesstoken=token) THEN
-          SELECT * FROM user_accounts WHERE user_accounts.accesstoken=token INTO found_user;
-          SELECT * FROM user_sessions WHERE user_sessions.id=found_user.currentsession
-                                            AND user_sessions.userid=found_user.id INTO user_session;
+        IF EXISTS (SELECT id FROM user_accounts WHERE access_token=token) THEN
+          SELECT * FROM user_accounts WHERE access_token=token INTO found_user;
+          SELECT * FROM user_sessions WHERE id=found_user.current_session
+                                            AND user_id=found_user.id INTO user_session;
 
           found_userId = found_user.id;
           found_userName = found_user.name;
 
-          IF (now() - user_session.lastcheck) > session_timeout THEN
+          IF (now() - user_session.last_check) > session_timeout THEN
             found_sessionId = user_session.id+1;
 
-            INSERT INTO user_sessions ("userid", "id") VALUES (found_user.id, found_sessionId);
+            INSERT INTO user_sessions (user_id, id) VALUES (found_user.id, found_sessionId);
 
-            UPDATE user_accounts SET currentSession=found_sessionId
+            UPDATE user_accounts SET current_session=found_sessionId
             WHERE id=found_user.id;
           ELSE
-            UPDATE user_sessions SET lastcheck=now()
-            WHERE id=found_user.currentsession AND userid=found_user.id;
+            UPDATE user_sessions SET last_check=now()
+            WHERE id=found_user.current_session AND user_id=found_user.id;
 
             found_sessionId = user_session.id;
           END IF;
         END IF;
-        RETURN QUERY SELECT found_userId, found_userName, found_sessionId;
+        RETURN QUERY SELECT found_userId, found_sessionId;
       END;
     $$
     LANGUAGE "plpgsql" VOLATILE;
 
 CREATE OR REPLACE FUNCTION update_access_token_and_ping_session(token VARCHAR, newToken VARCHAR)
   RETURNS TABLE (
-    user_id INTEGER,
-    user_name VARCHAR(255),
-    session_id INTEGER
+    userId INTEGER,
+    userName VARCHAR(255),
+    sessionId INTEGER
   )
-    as $$
+    AS $$
       DECLARE
 
       BEGIN
-        UPDATE user_accounts SET accesstoken=newToken WHERE accesstoken=token;
+        UPDATE user_accounts SET access_token=newToken WHERE access_token=token;
         RETURN QUERY SELECT * FROM ping_session_and_get_user_with_access_token(newToken);
       END;
     $$
