b0de80140f65c328a4dbe873be8d875d15d09a7d
chadrc <chadrcollins91@gmail.com>
2016-11-27 08:07:03 -0500

    Extracted out session updating in sql functions.

--- postgresimage/02-services-functions.sql
+++ postgresimage/02-services-functions.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION username_exists(username VARCHAR)
   RETURNS VARCHAR(255)
     AS $$
     DECLARE
-      user_name VARCHAR = '';
+      user_name VARCHAR := '';
     BEGIN
       IF EXISTS (SELECT name FROM user_accounts WHERE name=username) THEN
         SELECT name FROM user_accounts WHERE name=username INTO user_name;
@@ -24,6 +24,43 @@ CREATE OR REPLACE FUNCTION create_user(username VARCHAR, pass VARCHAR, token VAR
     $$
     LANGUAGE SQL;
 
+CREATE OR REPLACE FUNCTION update_session_with_access_token(token VARCHAR)
+  RETURNS TABLE (
+    userId INTEGER,
+    sessionId INTEGER
+  )
+  AS $$
+    DECLARE
+      found_userId INTEGER := 0;
+      found_sessionId INTEGER := 0;
+      session_timeout INTERVAL := 10 * INTERVAL '1 MINUTE';
+      found_user user_accounts;
+      user_session user_sessions;
+    BEGIN
+      SELECT * FROM user_accounts WHERE access_token=token INTO found_user;
+      SELECT * FROM user_sessions WHERE id=found_user.current_session
+                                        AND user_id=found_user.id INTO user_session;
+
+      found_userId = found_user.id;
+
+      IF (now() - user_session.last_check) > session_timeout THEN
+        found_sessionId = user_session.id+1;
+
+        INSERT INTO user_sessions (user_id, id) VALUES (found_user.id, found_sessionId);
+
+        UPDATE user_accounts SET current_session=found_sessionId
+        WHERE id=found_user.id;
+      ELSE
+        UPDATE user_sessions SET last_check=now()
+        WHERE id=found_user.current_session AND user_id=found_user.id;
+
+        found_sessionId = user_session.id;
+      END IF;
+      RETURN QUERY SELECT found_userId, found_sessionId;
+    END;
+  $$
+  LANGUAGE "plpgsql" VOLATILE;
+
 CREATE OR REPLACE FUNCTION ping_session_and_get_user_with_access_token(token VARCHAR)
   RETURNS TABLE (
     userId INTEGER,
@@ -33,30 +70,9 @@ CREATE OR REPLACE FUNCTION ping_session_and_get_user_with_access_token(token VAR
       DECLARE
         found_userId INTEGER := 0;
         found_sessionId INTEGER := 0;
-        session_timeout INTERVAL := 10 * INTERVAL '1 MINUTE';
-        found_user user_accounts;
-        user_session user_sessions;
       BEGIN
         IF EXISTS (SELECT id FROM user_accounts WHERE access_token=token) THEN
-          SELECT * FROM user_accounts WHERE access_token=token INTO found_user;
-          SELECT * FROM user_sessions WHERE id=found_user.current_session
-                                            AND user_id=found_user.id INTO user_session;
-
-          found_userId = found_user.id;
-
-          IF (now() - user_session.last_check) > session_timeout THEN
-            found_sessionId = user_session.id+1;
-
-            INSERT INTO user_sessions (user_id, id) VALUES (found_user.id, found_sessionId);
-
-            UPDATE user_accounts SET current_session=found_sessionId
-            WHERE id=found_user.id;
-          ELSE
-            UPDATE user_sessions SET last_check=now()
-            WHERE id=found_user.current_session AND user_id=found_user.id;
-
-            found_sessionId = user_session.id;
-          END IF;
+          SELECT * FROM update_session_with_access_token(token) INTO found_userId, found_sessionId;
         END IF;
         RETURN QUERY SELECT found_userId, found_sessionId;
       END;
@@ -70,8 +86,8 @@ CREATE OR REPLACE FUNCTION clear_access_token(token VARCHAR)
   )
     AS $$
       DECLARE
-        found_user_id INTEGER = 0;
-        found_session_id INTEGER = 0;
+        found_user_id INTEGER := 0;
+        found_session_id INTEGER := 0;
       BEGIN
         IF EXISTS(SELECT access_token FROM user_accounts WHERE access_token=token) THEN
           UPDATE user_accounts SET access_token='' WHERE access_token=token
@@ -107,7 +123,7 @@ CREATE OR REPLACE FUNCTION get_session_with_username_and_pass(username VARCHAR, 
             SELECT access_token FROM user_accounts WHERE name=username AND password=pass INTO found_token;
           END IF;
 
-          SELECT * FROM ping_session_and_get_user_with_access_token(found_token) INTO found_userId, found_sessionId;
+          SELECT * FROM update_session_with_access_token(found_token) INTO found_userId, found_sessionId;
           RETURN QUERY SELECT found_userId, found_sessionId, found_token;
         ELSE
           RETURN QUERY SELECT found_userId, found_sessionId, found_token;
